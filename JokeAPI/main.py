e1947286599a97dd255b6837236c8b75c3273361
Luke Roy <lukeroy@Lukes-MacBook-Pro.local>
2022-11-08 13:56:27 +0100

    Add Frontend color and comments

--- JokeAPI/main.py
+++ JokeAPI/main.py
@@ -118,7 +118,5 @@ def home():
     return render_template('index.html', joke=joke, isFunny=isFunny, percentage=percentage)
 
 
-
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
