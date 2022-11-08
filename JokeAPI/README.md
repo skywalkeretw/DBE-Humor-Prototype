7a0ab0b3e51aa1da917cacb5b6e84305ed881e1d
Luke Roy <lukeroy@Lukes-MacBook-Pro.local>
2022-11-08 11:56:45 +0100

    Add Frontend to system

--- JokeAPI/README.md
+++ JokeAPI/README.md
@@ -1,3 +1,8 @@
 # Joke detection api
 
 implemented as docker container using a flask api and tensorflow nlp
+
+This a a simple Flask API inside a docker container.
+The Base image for the container is `tensorflow/tensorflow`
+
+befor the first 
--- JokeAPI/main.py
+++ JokeAPI/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import json
 import tensorflow as tf
 import pandas as pd
@@ -19,19 +19,19 @@ training_size = 20000
 # Desired accuracy of the training
 DESIRED_ACCURACY = 0.922
 
-def isSarcastic(prediction, sentence):
+def isFunnyTxt(prediction):
     ret = ""
     if len(prediction) > 1:
-        for i, v in enumerate(prediction):
+        for  v in enumerate(prediction):
             if v > 0.5:
-                ret = "\n" + sentence[i] + " -> is Funny: " + str(v[0]*100) + "%"
+                ret =  "is Funny" + str(v[0]*100) + "%"
             else:
-                ret = "\n" + sentence[i] + " -> is Not Funny: " + str(v[0]*100) + "%"
+                ret = "is Not Funny" + str(v[0]*100) + "%"
     else:
         if prediction > 0.5:
-            ret = "\n" + sentence[0] + " -> is Funny: " + str(prediction[0]*100) + "%"
+            ret =  "is Funny"
         else:
-            ret = "\n" + sentence[0] + " -> is Not Funny: " + str(prediction[0]*100) + "%"
+            ret = "is Not Funny"
     
     return ret
 
@@ -85,17 +85,6 @@ def _setup():
     callbacks = myCallback()
     model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), verbose=2, callbacks=[callbacks])
 
-# Flask endpoints
-
-
-@app.route("/")
-def hello_world():
-    sentence = ["My IQ test results came back. They were negative."]
-    sequences = tokenizer.texts_to_sequences(sentence)
-    padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
-    isFunny = isSarcastic(model.predict(padded), sentence)
-    return isFunny
-
 
 #http://localhost:8082/isfunny/My%20IQ%20test%20results%20came%20back.%20They%20were%20negative.
 #endpoint to check if a joke passed is funny or not
@@ -106,8 +95,29 @@ def is_joke(joke):
     sentence.append(joke)
     sequences = tokenizer.texts_to_sequences(sentence)
     padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
-    isFunny = isSarcastic(model.predict(padded), sentence)
-    return isFunny
+    isFunny = isFunnyTxt(model.predict(padded))
+    return joke + " : " +isFunny
+
+
+@app.route('/', methods=['GET','POST'])
+@app.route('/home', methods=['GET','POST'])
+def home():
+    isFunny = ""
+    joke = ""
+    percentage = ""
+    if request.method == 'POST':
+        joke = request.form['joke']
+        sentence = []
+        print(joke)
+        sentence.append(joke)
+        sequences = tokenizer.texts_to_sequences(sentence)
+        padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+        prediction = model.predict(padded)
+        percentage = prediction[0][0]*100
+        isFunny = isFunnyTxt(prediction)
+    return render_template('index.html', joke=joke, isFunny=isFunny, percentage=percentage)
+
+
 
 
 if __name__ == '__main__':
